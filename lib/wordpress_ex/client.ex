93a9c338678ca1b2b1739cc82e1970f2d5a85a1f
Brad Hanks <hanks.brad@gmail.com>
2024-04-14 21:15:47 -0600

    error handling, encapsulation

--- lib/wordpress_ex/client.ex
+++ lib/wordpress_ex/client.ex
@@ -1,11 +1,12 @@
 defmodule WordpressEx.Client do
   @moduledoc false
-  @base_url Application.get_env(:wordpress_ex, :base_url, "")
-  @http_client Application.get_env(:wordpress_ex, :http_client, HTTPoison)
 
   @doc """
   GET endpoint that returns a parsed response
   """
+  defp base_url, do: Application.get_env(:wordpress_ex, :base_url, "")
+  defp http_client, do: Application.get_env(:wordpress_ex, :http_client, HTTPoison)
+
   def get(path, params) do
     response(:get, path, "", [], params: params)
     |> parse
@@ -15,19 +16,22 @@ defmodule WordpressEx.Client do
     parse_json(response.body)
   end
 
-  defp parse({_, response}) do
-    with {:ok, error_json} <- parse_json(response.body) do
-      {:error, error_json}
-    else
-      error -> {:error, error}
-    end
+  defp parse({:ok, response}) do
+    {:error, "Unexpected status code: #{response.status_code}"}
+  end
+
+  defp parse({:error, _reason} = error) do
+    error
   end
 
   defp parse_json(json) do
-    Jason.decode(json, keys: :atoms)
+    case Jason.decode(json) do
+      {:ok, data} -> data
+      error -> error
+    end
   end
 
   defp response(method, path, body, headers, options) do
-    @http_client.request(method, @base_url <> path, body, headers, options)
+    http_client().request(method, base_url() <> path, body, headers, options)
   end
 end
